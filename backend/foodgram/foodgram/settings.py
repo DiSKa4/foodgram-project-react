3c3bbb6585bf4a474dd30ee3afb1e7a4c938e655
Андрей Цыбульченко <tsybulchenko@yandex.ru>
2022-09-13 20:08:51 +0300

    fixed

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': '5432'
     }
